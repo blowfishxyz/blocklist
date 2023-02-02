7c99cabc0b9343dc3d15d3e98012a1456cb00d75
Seva Zhidkov <zhidkovseva@gmail.com>
2023-02-02 01:08:55 +0000

    add more ogs

--- src/bloomFilter.ts
+++ src/bloomFilter.ts
@@ -13,30 +13,41 @@ import type { BloomFilter } from "./types";
 // See encoding implementation in
 // https://github.com/contain-rs/bit-vec/blob/d15090df70f6499da2c9770942c8d39750cb1a21/src/lib.rs#L1120.
 export function lookup(bloomFilter: BloomFilter, key: string): boolean {
+  console.log('[lookup]', key);
   // `k` means number of hash functions used for each lookup/insert.
   for (let i = 0; i < bloomFilter.k; i++) {
     // We use salt to be able to change bloom-filter ordering when needed.
     const hash = sha1(`${bloomFilter.salt}_${key}_${i}`, { asBytes: true });
+    console.log('[hash]', hash, i);
     // We only need first 4 bytes of the hash to construct a 32-bit integer.
     const buffer = Buffer.from(hash).subarray(0, 4);
+    console.log('[buffer]', buffer);
     // Determine bit vector position for bit that we're seeking. Each integer in bloomFilter.bitVector has 8 bits.
     const index = buffer.readUInt32BE(0) % bloomFilter.bits;
+    console.log('[big_endian_int]', buffer.readUInt32BE(0));
+    console.log('[bits]', bloomFilter.bits);
+    console.log('[index]', index);
     // Base64 encodes 24 bits (3 bytes) in 4 characters (4 bytes).
     // So, we need extract a 24-bit base64-encoded sequence, which is basically a 4 character string slice.
     const sequenceIndex = Math.floor(index / 24);
+    console.log('[sequenceIndex]', sequenceIndex);
     const sequence = Buffer.from(
       bloomFilter.bitVector.slice(sequenceIndex * 4, sequenceIndex * 4 + 4),
       "base64"
     );
+    console.log('[sequence]', sequence);
     // Within this sequence, we need extract just one bit.
     const bitInSequence = index % 24;
+    console.log('[bitInSequence]', bitInSequence);
     // This calculates specific byte & bit we are looking for in the 24-bit sequence.
     const byte = Math.floor(bitInSequence / 8);
     const bit = bitInSequence % 8;
+    console.log('[byte/bit]', byte, bit);
     // Lookup bit in the bit vector using a bit mask.
     // We invert `bit` position, because `bit` is counted from highest order bit.
     // 1 << 7 selects highest order bit, 1 << 0 selects lowest order bit.
     if ((sequence[byte] & (1 << (7 - bit))) === 0) {
+      console.log('[return]', byte, bit);
       // If lookup isn't successful, the item is definitely not in the bloom filter.
       return false;
     }
