d6186122798b42a8a78c7eb7c6b0e28497c002ac
Seva Zhidkov <seva@phantom.app>
2025-06-30 17:43:24 +0100

    chore: remove unused backend client with persistence support

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,3 @@
-import {
-  DownloadBlocklistRequestAllowListsEnum,
-  DownloadBlocklistRequestBlockListsEnum,
-  DownloadBlocklistRequestPriorityAllowListsEnum,
-  DownloadBlocklistRequestPriorityBlockListsEnum,
-} from "./blowfishLocalBlocklist";
-
 export type ApiConfig = {
   basePath: string;
   apiKey?: string;
@@ -24,24 +17,6 @@ export type BloomFilter = {
   bits: number;
   salt: string;
 };
-
-export enum BlowfishBlocklistStorageKey {
-  DomainBlocklist = "BF:DOMAIN_BLOCKLIST:0.0.7",
-  BloomFilter = "BF:BLOCKLIST_BLOOM_FILTER:0.0.7",
-  UserAllowlist = "BF:USER_ALLOWLIST:0.0.7",
-}
-
-export interface BlowfishBlocklistStorage {
-  getItem: <T>(key: BlowfishBlocklistStorageKey) => Promise<T | undefined>;
-  setItem: <T>(key: BlowfishBlocklistStorageKey, data: T) => Promise<void>;
-}
-
-export type BlockListsEnum = DownloadBlocklistRequestBlockListsEnum;
-export type AllowListsEnum = DownloadBlocklistRequestAllowListsEnum;
-export type PriorityBlockListsEnum =
-  DownloadBlocklistRequestPriorityBlockListsEnum;
-export type PriorityAllowListsEnum =
-  DownloadBlocklistRequestPriorityAllowListsEnum;
 
 export enum Action {
   BLOCK = "BLOCK",
